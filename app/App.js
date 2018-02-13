c5fea568622acbdd57ce53968a039afc4f0bfdc7
Fabrice <fabrice.hong@gmail.com>
2018-02-13 17:51:53 +0100

    component state

--- app/App.js
+++ app/App.js
@@ -3,17 +3,58 @@ import React, {Component} from 'react';
 import { Platform, Text, View, StyleSheet, TextInput, ScrollView, CheckBox } from 'react-native';
 import MyCheckBox from './MyCheckBox';
 
+let currentTaskId = 0;
 export default class App extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          id: currentTaskId++,
+          text: "prepare presentation",
+          isDone: true,
+        },
+        {
+          id: currentTaskId++,
+          text: "book restaurant",
+          isDone: false
+        },
+        {
+          id: currentTaskId++,
+          text: "get a beer",
+          isDone: false
+        },
+      ]
+    };
+  }
+
+  switchChecked(taskId) {
+    this.setState({
+      ...this.state,
+      tasks: this.state.tasks.map(task => ({
+          ...task,
+          isDone: task.id == taskId
+            ? !task.isDone
+            : task.isDone
+        }))
+    });
+  }
+
   render() {
+    const tasks = this.state.tasks;
     return (
       <View style={styles.container}>
         <Text style={styles.title} >todos</Text>
         <TextInput style={[styles.text, {marginBottom:10}]} autoCorrect={false}
           placeholder="What needs to be done ?"/>
         <ScrollView>
-          <Task text='prepare presentation' isDone={true} />
-          <Task text='book restaurant' isDone={false} />
+          {
+            tasks.map((task =>
+              <Task key={task.id} onValueChange={_ => this.switchChecked(task.id)} {...task} />
+              )
+            )
+          }
         </ScrollView>
       </View>
 
@@ -22,10 +63,10 @@ export default class App extends Component {
 }
 
 const Task = (props)=>{
-  const {text, isDone} = props;
+  const {text, isDone, onValueChange} = props;
   return (
     <View style={{flexDirection: 'row', margin:10}} >
-      <MyCheckBox value={isDone} />
+      <MyCheckBox value={isDone} onValueChange={onValueChange}/>
       <Text style={[styles.text, {color : isDone ? '#bbb' : '#888', textDecorationLine: isDone ? 'line-through' : null}]} >{text}</Text>
     </View>
   )
