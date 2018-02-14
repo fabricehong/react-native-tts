4774104d665e72df5147963b3899a5ed055009a6
Fabrice <fabrice.hong@gmail.com>
2018-02-14 07:36:18 +0100

    adding 3rd party lib

--- app/App.js
+++ app/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 
-import { Platform, Text, View, StyleSheet, TextInput, ScrollView, CheckBox } from 'react-native';
+import { Platform, Text, View, StyleSheet, TextInput, ScrollView, CheckBox, Button } from 'react-native';
 import MyCheckBox from './MyCheckBox';
+import Tts from 'react-native-tts';
 
 let currentTaskId = 0;
 export default class App extends Component {
@@ -11,6 +12,17 @@ export default class App extends Component {
     this.state = {
       tasks: []
     };
+  }
+  componentDidMount() {
+    Tts.getInitStatus().then(_=>{
+      this.setState({
+        ...this.state,
+        ready: true
+      });
+      Tts.addEventListener('tts-start', (event) => console.log("start", event));
+      Tts.addEventListener('tts-finish', (event) => console.log("finish", event));
+      Tts.addEventListener('tts-cancel', (event) => console.log("cancel", event));
+    });
   }
 
   switchChecked(taskId) {
@@ -40,11 +52,25 @@ export default class App extends Component {
     });
   }
 
+  sayTasks() {
+    const doneTasks = this.state.tasks.filter(task => task.isDone).map(task => task.text);
+    const undoneTasks = this.state.tasks.filter(task => !task.isDone).map(task => task.text);
+
+    const message = `
+      What I did yesterday was:
+      ${doneTasks.length == 0 ? 'nothing' : doneTasks.join(", ")}
+      
+      What I will do today is:
+      ${undoneTasks.length == 0 ? 'nothing' : undoneTasks.join(", ")}
+    `;
+    Tts.speak(message);
+  }
+
   render() {
     const tasks = this.state.tasks;
     return (
       <View style={styles.container}>
-        <Text style={styles.title} >todos</Text>
+        <Text style={styles.title} >My daily standup</Text>
         <TextInput style={[styles.text, {marginBottom:10}]} autoCorrect={false}
             onSubmitEditing={(event) => this.addTask()}
             onChangeText={text =>
@@ -62,6 +88,9 @@ export default class App extends Component {
             )
           }
         </ScrollView>
+        {this.state.ready
+          ? <Button title="Do the daily !" onPress={_ => this.sayTasks()}/>
+          : null}
       </View>
 
     );
