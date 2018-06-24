2fc8e2ada488cf48c6dfeec7250a7f26d7a947b9
Fabrice <fabrice.hong@gmail.com>
2018-06-24 20:14:11 +0200

    fix task detail parameter
    temp
    
    fix task detail parameter

--- app/TodoDetail.js
+++ app/TodoDetail.js
@@ -1,16 +1,50 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { Container, Textarea, View, Content } from 'native-base';
+import { Container, Textarea, View, Content, Input, Item } from 'native-base';
 
-const TodoDetail = props => (
-    <Container >
-        <View style={styles.content} >
-            <Content >
-                <Textarea rowSpan={5} bordered placeholder="Textarea" value={props.value} onChangeText={props.onChangeText} />
-            </Content>
-        </View>
-    </Container>
-)
+class TodoDetail extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.task = props.navigation.state.params.task;
+        // this.state = {};
+        this.state = {
+            text: this.task.text,
+            notes: this.task.notes,
+        };
+    }
+
+    onChangeText = text => {
+        this.task.text = text;
+        this.setState({text});
+    }
+
+    onChangeNotes = notes => {
+        this.task.notes = notes;
+        this.setState({notes});
+    }
+
+    render() {
+        const { text, notes } = this.state;
+        return (
+            <Container >
+                <View style={styles.content} >
+                    <Content >
+                        <Item regular>
+                            <Input
+                                autoCorrect={false}
+                                placeholder="What needs to be done ?"
+                                value={text}
+                                onChangeText={this.onChangeText}
+                            />
+                        </Item>
+                        <Textarea rowSpan={5} bordered placeholder="Notes" value={notes} onChangeText={this.onChangeNotes} />
+                    </Content>
+                </View>
+            </Container>
+        );
+    }
+}
 
 const styles = StyleSheet.create(
     {
